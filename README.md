a6eaa55a4f4d85b513798434d5359f87526906a0
Vani Aggarwal <vaniaggarwal96@gmail.com>
2024-12-03 23:14:39 +0530

    calculate done

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ another web calculator; as if google doesn't already have a gazillion of these
   // display numbers on the display tab as the numbers are clicked
   // the buttons changes its colour as clicked
   // the calc resets to 0 
--SOLVE THE CALCULATION
-  indentify the type of key ; number, operator, decimal , eqauls to
-  
+-HANDLING THE BUTTONS
+ - NUMBER AND OPERATORS
+   // indentify the type of key ; number, operator, decimal , eqauls to
+   // console.log all the five categories to the respective buttons
+   // display the number in the display
+   // if there's already a number ; + new number else replace 0 ; or if the previous clicked button was an operator
+ - 
+
+
--- index.js
+++ index.js
@@ -5,42 +5,77 @@ const keys = document.querySelector(".keys");
 
 keys.addEventListener('click' , e => {
     if (e.target.matches("button")){
-       let key = e.target;
-       let action = key.dataset.action;
-       let keyContent = key.textContent;
-       let dislayedNum = display.textContent;
-       let previousKeyType = container.dataset.previousKeyType;
+       const key = e.target;
+       const action = key.dataset.action;
+       const keyContent = key.textContent;
+       let displayedNum = display.textContent;
+       const previousKeyType = container.dataset.previousKeyType;
 
 
         if(!action){
             console.log('number key!');
-            if (dislayedNum === '0' || previousKeyType === 'operator'){
+            if (displayedNum === '0' || previousKeyType === "operator") {
                 display.textContent = keyContent;
-            }else {
-                display.textContent = dislayedNum + keyContent;
+                container.dataset.previousKeyType = "number";
+            } else {
+                display.textContent = displayedNum + keyContent;
             }
+            
+            
         }
-
-
 
         if(action === 'divide' ||
             action === 'multiply' ||
             action === "minus" || 
             action === 'plus'){
             console.log("operator key!!")
-            container.dataset.previousKeyType = 'operator';
+            container.dataset.previousKeyType = "operator";
+            container.dataset.firstValue = displayedNum;
+            container.dataset.operator = action;
 
         }
 
         if (action === 'clear'){
             console.log('clear key!')
+            display.textContent = '0';
         }
         if(action === 'equal'){
-            console.log('equal key!')
+            console.log('equal key!');
+            const firstValue = container.dataset.firstValue;
+            const operator = container.dataset.operator;
+            const secondValue = displayedNum;
+            
+            const calculate = (a, operator ,b) => {
+                switch(operator){
+                    case 'divide':
+                        result = parseInt(a)/ parseInt(b) ;
+                        break;
+                    case 'multiply':
+                        result = parseInt(a)*parseInt(b);
+                        break;
+                    case 'minus':
+                        result = parseInt(a)-parseInt(b);
+                        break;
+                    case 'plus':
+                        result = parseInt(a)+parseInt(b); 
+                        break;   
+                }  
+
+                console.log(firstValue , operator , secondValue)
+                console.log(result)
+
+                display.textContent = result
+                return result;
+    
+            }
+            calculate(firstValue , operator , secondValue);
+
+
+
         }
         if(action === 'decimal'){
             console.log('decimal key!!')
-            display.textContent = dislayedNum + '.';
+            display.textContent = displayedNum + '.';
         }
     }
 })
