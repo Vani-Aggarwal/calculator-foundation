dceb6ff105de945787634347cc083dfef17daf27
Vani Aggarwal <vaniaggarwal96@gmail.com>
2024-12-03 21:15:27 +0530

    numbers and operators done

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # calculator-foundation
 another web calculator; as if google doesn't already have a gazillion of these
 -BOILERPLATE
-   create html,js,css
-   create a div in the middle of  the page
-   400*600
-   display numbers on the display tab as the numbers are clicked
-   the buttons changes its colour as clicked
-   the calc resets to 0 
+  // create html,js,css
+  // create a div in the middle of  the page
+  // 400*600
+  // display numbers on the display tab as the numbers are clicked
+  // the buttons changes its colour as clicked
+  // the calc resets to 0 
+-SOLVE THE CALCULATION
+  indentify the type of key ; number, operator, decimal , eqauls to
+  
--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const numbers = document.querySelectorAll(".numbers");
+const container = document.querySelector(".container")
 const display = document.querySelector(".display");
 const clear = document.querySelector(".clear");
+const keys = document.querySelector(".keys");
+
+keys.addEventListener('click' , e => {
+    if (e.target.matches("button")){
+       let key = e.target;
+       let action = key.dataset.action;
+       let keyContent = key.textContent;
+       let dislayedNum = display.textContent;
+       let previousKeyType = container.dataset.previousKeyType;
 
 
-numbers.forEach((number) => {
-    number.addEventListener('click' , () => {
-    display.textContent = number.innerHTML;
-    console.log(number.textContent)})
+        if(!action){
+            console.log('number key!');
+            if (dislayedNum === '0' || previousKeyType === 'operator'){
+                display.textContent = keyContent;
+            }else {
+                display.textContent = dislayedNum + keyContent;
+            }
+        }
+
+
+
+        if(action === 'divide' ||
+            action === 'multiply' ||
+            action === "minus" || 
+            action === 'plus'){
+            console.log("operator key!!")
+            container.dataset.previousKeyType = 'operator';
+
+        }
+
+        if (action === 'clear'){
+            console.log('clear key!')
+        }
+        if(action === 'equal'){
+            console.log('equal key!')
+        }
+        if(action === 'decimal'){
+            console.log('decimal key!!')
+            display.textContent = dislayedNum + '.';
+        }
+    }
 })
-clear.addEventListener('click' , ()=>{
-    display.textContent = 0;
-})
+
